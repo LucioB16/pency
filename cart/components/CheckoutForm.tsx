1c361832398cd93eecd64eadc0d41a077a2c08e1
LucioB16 <luciobotacin@gmail.com>
2020-05-18 17:48:27 -0300

    add checkout form

---
+++ cart/components/CheckoutForm.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+
+import {
+    Stack,
+    Divider,
+    Select,
+    Input,
+  } from "@chakra-ui/core";
+
+
+import { useCart } from "../hooks";
+import FormControl from "~/ui/controls/FormControl";
+import { Checkout } from "../types";
+
+interface Props {
+    defaultValues?: Checkout
+    onSubmit: (values: Checkout) => void;
+    children: (options: {
+        form: JSX.Element;
+        isLoading: boolean;
+        submit: (e?: React.BaseSyntheticEvent<object, any, any> | undefined) => Promise<void>;
+    }) => JSX.Element;
+}
+
+const CheckoutForm: React.FC<Props> = ({ defaultValues = {}, children, onSubmit }) => {
+    const {total, MEDIOS_PAGO, checkout, selectPago} = useCart();
+    
+    const { handleSubmit: submit, errors, register, control, formState } = useForm<Checkout>({
+        defaultValues,
+    });
+
+    function handleSubmit(values: Checkout) {
+        const checkout = {...defaultValues, ...values};
+
+        return onSubmit(checkout);
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLSelectElement>){
+        if(event.target.value.toString() == "Efectivo"){
+            
+        }
+    }
+
+    return children({
+        isLoading: formState.isSubmitting,
+        submit: submit(handleSubmit),
+        form: (
+            <form onSubmit={submit(handleSubmit)}>
+                <Stack spacing={4} width="100%">
+                    <Divider />
+                    <FormControl
+                        label="Medio de pago"
+                        name="pago"
+                    >
+                        <Select ref={register} name="pago" onChange={handleChange} placeholder="Seleccioná un medio de pago">
+                            {MEDIOS_PAGO.map(({ id, nombre }) => (
+                                <option key={id} value={id}>{nombre}</option>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl
+                        isRequired
+                        error={errors.monto && (errors.monto.message || "Este campo es inválido")}
+                        isInvalid={Boolean(errors.monto)}
+                        label="Monto"
+                        name="monto"
+                    >
+                        <Input
+                            ref={register({required: true, pattern: /^[0-9]+(\,[0-9]{1,2})?$/})} 
+                            name="monto"
+                            placeholder="Con cuanto vas a pagar?"
+                            min={total}
+                            type="number"
+                        />
+                    </FormControl>
+                </Stack>
+            </form>
+        ),
+    })
+};
+
+export default CheckoutForm;
